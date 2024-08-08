2844be33369c54130663479c0e9e40cc75866d4e
ashleyjcast <ashleyj.castaneda@gmail.com>
2024-08-08 16:33:13 -0400

    Final

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,12 +15,13 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     csv_header = next(csvreader) 
 
-# Calculate total votes and assign candidate name row
+# Process each row
+# The total number of votes each candidate won
     for row in csvreader:
         total_votes += 1
         candidate_name = row[2]
 
-# Create list of names 
+# A complete list of candidates who received votes 
         if candidate_name not in names:
             names.append(candidate_name)
         if candidate_name in candidate_vote:
@@ -29,13 +30,13 @@ with open(csvpath) as csvfile:
             candidate_vote[candidate_name] =1
 
 
-# Calculate percentage of votes
+# The percentage of votes each candidate won
 results = []
 for candidate, votes in candidate_vote.items():
     percentage = (votes / total_votes) * 100
     results.append(f'{candidate}: {percentage:.3f}% ({votes})')
 
-# Calculate winner 
+# The winner of the election based on popular vote 
 winner = max(candidate_vote, key=candidate_vote.get)
 
 # Print Results
@@ -48,3 +49,16 @@ for result in results:
 print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
+
+# Create a text file of the results to add to analysis folder
+output_path = os.path.join('analysis', 'election_results.txt')
+with open(output_path, 'w') as file:
+    file.write("Election Results\n")
+    file.write("----------------------------")
+    file.write(f"Total Votes: {total_votes}\n")
+    file.write("----------------------------")
+    for result in results:
+        file.write(result + '\n')
+    file.write("----------------------------")
+    file.write(f"Winner: {winner}\n")
+    file.write("----------------------------")
